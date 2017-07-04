9795c35c060bcf0dd10be0ed047468843ddcc75b
Ben Weedon <ben.weedon@outlook.com>
2017-07-04 01:13:32 -0700

    Add timer to keep track of performance

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,12 @@ extern crate rusteuler;
 use std::env;
 use std::io::prelude::*;
 use std::process;
+use std::time;
 
 fn main() {
+    // start timing performance
+    let start_time = time::SystemTime::now();
+
     let mut args = env::args();
     args.next(); // drop the first argument
     let mut stderr = std::io::stderr();
@@ -17,7 +21,7 @@ fn main() {
     let problem_number = problem_number.parse().unwrap_or_else(|_| {
         writeln!(
             &mut stderr,
-            "The value {} is not a valid number.",
+            "The value \"{}\" is not a valid number.",
             problem_number
         ).expect("could not write to stderr");
         process::exit(1);
@@ -30,7 +34,19 @@ fn main() {
                 &mut stderr,
                 "An error was encountered running the problem: {}",
                 err
-            ).expect("could not write to stderr")
+            ).expect("could not write to stderr");
+            process::exit(1);
         }
     }
+
+    // display the elapsed time
+    let elapsed_time = start_time.elapsed().unwrap_or_else(|err| {
+        writeln!(&mut stderr, "Failed to get elapsed time: {}", err)
+            .expect("could not write to stderr");
+        process::exit(1);
+    });
+    println!(
+        "Solution computed in {} seconds.",
+        (elapsed_time.as_secs() as f64) + (elapsed_time.subsec_nanos() as f64 * 0.000000001)
+    );
 }
