e1a0ad66a5f697aea3e45e428549ce3c58688a6f
Ben Weedon <ben.weedon@outlook.com>
2018-01-29 21:20:44 -0800

    Improve speed of problem 5 with threads
    Went from about 80 second to about 50 seconds, but still clearly needs a
    lot of work.

--- src/problem_5.rs
+++ src/problem_5.rs
@@ -1,7 +1,44 @@
+use std::sync::{Arc, Mutex};
+use std::thread;
+
 pub fn problem_5() -> Result<u64, String> {
-    (20..)
-        .find(|n| (2..21).all(|d| n % d == 0))
-        .ok_or(String::from("This should never be reached."))
+    let current_bound = Arc::new(Mutex::new(20));
+    let lowest_result = Arc::new(Mutex::new(u64::max_value()));
+    const NUM_THREADS: u64 = 4;
+    let threads = (0..NUM_THREADS).map(|_| {
+        let current_bound = Arc::clone(&current_bound);
+        let lowest_result = Arc::clone(&lowest_result);
+        thread::spawn(move || loop {
+            let low_bound: u64;
+            let high_bound: u64;
+            {
+                let mut bound = current_bound.lock().unwrap();
+                low_bound = *bound;
+                *bound += 10000;
+                high_bound = *bound;
+            }
+
+            let opt = (low_bound..high_bound).find(|n| (2..21).all(|d| n % d == 0));
+            {
+                let mut lowest = lowest_result.lock().unwrap();
+                match opt {
+                    Some(v) => if v < *lowest {
+                        *lowest = v;
+                    },
+                    None => (),
+                }
+                if *lowest < u64::max_value() {
+                    break;
+                }
+            }
+        })
+    });
+
+    for t in threads {
+        t.join().unwrap();
+    }
+    let result = *lowest_result.lock().unwrap();
+    Ok(result)
 }
 
 #[cfg(test)]
