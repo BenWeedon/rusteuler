e3e7670c4bd1f80dc08738c8fa95a9738fcd0fbb
Ben Weedon <ben@weedon.email>
2020-12-06 14:26:35 -0800

    Improve the efficiency of solution 3 significantly
    Iterating to the square root seems to use far fewer iterations for this
    problem.

--- src/problem_3.rs
+++ src/problem_3.rs
@@ -2,26 +2,36 @@
 //! >
 //! > What is the largest prime factor of the number 600851475143 ?
 //!
-//! This solution is currently very inefficient. It iterates through every
-//! factor of 600851475143 and checks if each one is prime.
-//! [`util::is_prime`](crate::util::is_prime) is also iterative, making this
-//! algorithm extremely slow.
+//! For this solution, we find all the factors of 600851475143. We iterate up
+//! until its square root, and for each number test both it and its other
+//! associated factor. We test the factors by checking if they're prime using
+//! [`util::is_prime`](crate::util::is_prime) and then checking if they're
+//! larger than our current largest factor.
+//!
+//! This is based off one of the solutions at
+//! https://www.mathblog.dk/project-euler-problem-3/.
 //!
 //! ```
 //! # rusteuler::framework::run_solution(3, || {
 //! use rusteuler::util;
 //!
 //! const N: u64 = 600851475143;
+//! let n_sqrt = (N as f64).sqrt() as u64;
 //! let mut solution = 0;
-//! for n in 1..N / 2 {
-//!     if N % n == 0 {
-//!         let factor = N / n;
-//!         if util::is_prime(factor) {
-//!             solution = factor;
-//!             break;
+//!
+//! for i in 2..n_sqrt {
+//!     if N % i == 0 {
+//!         let lower_divisor = i;
+//!         let higher_divisor = N / i;
+//!
+//!         if util::is_prime(higher_divisor) && higher_divisor > solution {
+//!             solution = higher_divisor;
+//!         } else if util::is_prime(lower_divisor) && lower_divisor > solution {
+//!             solution = lower_divisor;
 //!         }
 //!     }
 //! }
+//!
 //! assert_eq!(solution, 6857);
 //! # Ok(())
 //! # }).unwrap();
