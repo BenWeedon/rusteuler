38178d3cdb48b67ca0fe40a93a1c44b97e690515
Ben Weedon <ben@weedon.email>
2020-12-06 14:50:21 -0800

    Improve efficiency of solution 3 even more
    Now we just keep factoring the number down until left with a prime. At
    that point, the remainder must be the largest prime factor.

--- src/problem_3.rs
+++ src/problem_3.rs
@@ -2,37 +2,29 @@
 //! >
 //! > What is the largest prime factor of the number 600851475143 ?
 //!
-//! For this solution, we find all the factors of 600851475143. We iterate up
-//! until its square root, and for each number test both it and its other
-//! associated factor. We test the factors by checking if they're prime using
-//! [`util::is_prime`](crate::util::is_prime) and then checking if they're
-//! larger than our current largest factor.
+//! For this solution, we simply keep dividing 600851475143 down until the
+//! remainder is prime. At that point all attempted divisors have been less
+//! than the square root of the remainder, meaning the remainder must be the
+//! largest prime factor.
 //!
 //! This is based off one of the solutions at
 //! https://www.mathblog.dk/project-euler-problem-3/.
 //!
 //! ```
 //! # rusteuler::framework::run_solution(3, || {
-//! use rusteuler::util;
-//!
 //! const N: u64 = 600851475143;
-//! let n_sqrt = (N as f64).sqrt() as u64;
-//! let mut solution = 0;
 //!
-//! for i in 2..n_sqrt {
-//!     if N % i == 0 {
-//!         let lower_divisor = i;
-//!         let higher_divisor = N / i;
-//!
-//!         if util::is_prime(higher_divisor) && higher_divisor > solution {
-//!             solution = higher_divisor;
-//!         } else if util::is_prime(lower_divisor) && lower_divisor > solution {
-//!             solution = lower_divisor;
-//!         }
+//! let mut remainder = N;
+//! let mut factor = 2;
+//! while factor * factor <= remainder {
+//!     if remainder % factor == 0 {
+//!         remainder = remainder / factor;
+//!     } else {
+//!         factor += 1;
 //!     }
 //! }
 //!
-//! assert_eq!(solution, 6857);
+//! assert_eq!(remainder, 6857);
 //! # Ok(())
 //! # }).unwrap();
 //! ```
