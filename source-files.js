aae505318732d2609f7acadfb3d4636dcae37949
binyomen <binyomen@users.noreply.github.com>
2020-12-08 04:23:50 +0000

    deploy: 75257d775bb755288739085ce98a9a274d0075ec

--- source-files.js
+++ source-files.js
@@ -1,3 +1,3 @@
 var N = null;var sourcesIndex = {};
-sourcesIndex["rusteuler"] = {"name":"","files":["framework.rs","lib.rs","problem_1.rs","problem_2.rs","problem_3.rs","problem_4.rs","problem_5.rs","problem_6.rs","problem_7.rs","util.rs"]};
+sourcesIndex["rusteuler"] = {"name":"","files":["framework.rs","lib.rs","problem_1.rs","problem_2.rs","problem_3.rs","problem_4.rs","problem_5.rs","problem_6.rs","problem_7.rs","problem_8.rs","util.rs"]};
 createSourceSidebar();
