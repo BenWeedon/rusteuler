24a80d6c8dd6c45ec2f4256568bb80f056daf9db
Ben Weedon <ben@weedon.email>
2020-12-07 22:57:43 -0800

    Improve the performance of solution 9
    Previously, we were doing a couple things wrong. First, we were
    iterating through all possible values of `c` even though once we have an
    `a` and a `b` we can just subtract them to get `c`. Second, we didn't
    need to iterate through values of `b` up to 1000 since `a + b` can't be
    more than 1000.

--- src/problem_9.rs
+++ src/problem_9.rs
@@ -8,13 +8,17 @@
 //! > There exists exactly one Pythagorean triplet for which `a + b + c =
 //! 1000`. Find the product `abc`.
 //!
-//! We iterate over all values of <var>a</var>, <var>b</var>, and <var>c</var>
-//! less than 1000. Once we find a combination where both <code>a<sup>2</sup> + b<sup>2</sup> = c<sup>2</sup></code>
-//! and <code>a + b + c = 1000</code>, we break out of all the loops with the
-//! solution.
+//! We iterate over all values of <var>a</var> from 0 to 1000, covering all
+//! natural numbers. Then, since <var>b</var> must be greater than <var>a</var>
+//! and <var>a</var> plus <var>b</var> can't be more than 1000, we iterate
+//! <var>b</var> from `a + 1` to `1000 - a`. Technically, we could also account
+//! for <var>c</var> being bigger than <var>b</var> in this iteration and set
+//! the top limit even lower, but that isn't done here.
 //!
-//! This solution is pretty inefficient since it needs to iterate over so many
-//! numbers.
+//! Once we have values of <var>a</var> and <var>b</var>, we can subtract them
+//! from 1000 to get <var>c</var>. Then it's just a matter of checking that
+//! <var>c</var> is greater than <var>b</var> and that they're a Pythagorean
+//! triplet, and we can break out of all the loops with the solution.
 //!
 //! ```
 //! # rusteuler::framework::run_solution(9, || {
@@ -22,13 +26,11 @@
 //!
 //! let mut solution = 0;
 //! 'outer: for a in 0..N {
-//!     for b in a + 1..N {
-//!         let c_squared = a.pow(2) + b.pow(2);
-//!         for c in b + 1..N {
-//!             if c.pow(2) == c_squared && a + b + c == N {
-//!                 solution = a * b * c;
-//!                 break 'outer;
-//!             }
+//!     for b in a + 1..N - a {
+//!         let c = N - a - b;
+//!         if c > b && a.pow(2) + b.pow(2) == c.pow(2) {
+//!             solution = a * b * c;
+//!             break 'outer;
 //!         }
 //!     }
 //! }
